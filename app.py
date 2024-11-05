db19ec195b8c5b21a5a7124906a4c1bc2cb41c41
hkvoluntary <97347174+hkvoluntary@users.noreply.github.com>
2024-11-04 23:11:18 -0800

    Update app.py

--- app.py
+++ app.py
@@ -124,4 +124,5 @@ def delete_user(user_id):
 # Run Flask Application
 # ============================
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
